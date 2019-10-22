17f8e0c68423fa7b59f7bda8eaf6638907371b59
tungnguyen <tungnguyentp@gmail.com>
2019-10-22 16:05:55 +0700

    - commit 123

--- RxDemo/RxDemo/ViewController.swift
+++ RxDemo/RxDemo/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtPass: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblTitle.text = R.string.localizable.loginLogin()
     }
 
 

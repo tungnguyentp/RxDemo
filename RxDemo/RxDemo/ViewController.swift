8e602749bdd75585fde13d45aa64fdb1f84861cf
Tung. Nguyen Huu - CMC Global DU3.21 <nhtung2@cmcglobal.vn>
2022-12-02 15:38:33 +0700

    edit text

--- RxDemo/RxDemo/ViewController.swift
+++ RxDemo/RxDemo/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         lblTitle.text = R.string.localizable.loginLogin()
+        //
     }
 
 
